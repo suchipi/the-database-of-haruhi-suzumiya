2c840ce47d33ede9efc5a85d53c91ad161a5ae24
Lily Skye <me@suchipi.com>
2023-11-08 06:01:35 -0700

    various funny cli options

--- src/haruhi.ts
+++ src/haruhi.ts
@@ -1,6 +1,27 @@
 #!/usr/bin/env yavascript
 import { loadNouns } from "./nouns";
 
+const { flags, args } = parseScriptArgs({
+  timer: number,
+  volume: number,
+  json: boolean,
+  help: boolean,
+  h: boolean,
+});
+
+if (flags.help || flags.h) {
+  console.log(
+    `
+Usage: haruhi.ts [options] [nouns...]
+Options:
+  --timer (number): print an entry once every n milliseconds
+  --volume (number): print a specific volume (ie. book number)
+  --json (boolean): output json
+`.trim()
+  );
+  std.exit(0);
+}
+
 function pickRandom<T>(array: Array<T>): { value: T; index: number } {
   const index = Math.floor(Math.random() * array.length);
   return { value: array[index], index };
@@ -19,10 +40,36 @@ function makeTitle(noun: string, nounIndex: number) {
   return `Volume ${nounIndex + 1}: The ${capitalize(noun)} of Haruhi Suzumiya`;
 }
 
+function printNoun(noun: string, index: number) {
+  const title = makeTitle(noun, index);
+  if (flags.json) {
+    echo(JSON.stringify({ title }));
+  } else {
+    echo(title);
+  }
+}
+
 const nouns = loadNouns();
 function printPick() {
   const { value: noun, index } = pickRandom(nouns);
-  echo(makeTitle(noun, index));
+  printNoun(noun, index);
 }
 
-setInterval(printPick, 1000);
+if (flags.timer != null) {
+  setInterval(printPick, flags.timer);
+} else if (flags.volume != null) {
+  const index = flags.volume - 1;
+  const noun = nouns[index];
+  if (!noun) {
+    console.error(red("No such volume: " + flags.volume));
+  } else {
+    printNoun(noun, index);
+  }
+} else if (args.length > 0) {
+  for (const arg of args) {
+    const fakeIndex = Math.floor(Math.random() * 10000 + nouns.length);
+    printNoun(arg, fakeIndex);
+  }
+} else {
+  printPick();
+}
